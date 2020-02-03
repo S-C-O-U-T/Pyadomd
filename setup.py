dd2fa4f83adb4169a6de210ed499aaec37127793
Andreas Boye <andboye@outlook.com>
2020-02-03 14:59:18 +0700

    requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
   name = 'pyadomd',         # How you named your package folder (MyLib)
   packages = ['pyadomd'],   # Chose the same as "name"
-  version = '0.0.4',      # Start with a small number and increase it with every change you make
+  version = '0.0.5',      # Start with a small number and increase it with every change you make
   license='Apache License 2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A pythonic approach to query SSAS data models',   # Give a short description about your library
   long_description=long_description,
@@ -17,7 +17,7 @@ setup(
   author = 'SCOUT',                   # Type in your name
   author_email = 'andboye@outlook.com',      # Type in your E-Mail
   url = 'https://github.com/Andboye/Pyadomd',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Andboye/Pyadomd/archive/v_0.0.2.tar.gz', 
+  download_url = 'https://github.com/Andboye/Pyadomd/archive/v_0.0.4.tar.gz', 
   keywords = ['adomd', 'ssas', 'ssas-tabular', 'ssasadomd'],   # Keywords that define your package best
   install_requires=[            
           'pythonnet'
